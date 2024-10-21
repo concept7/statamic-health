fb1cf916be4b75152eac96de6c6b52e7db25093b
Martijn Wagena <martijn@concept7.nl>
2024-10-21 15:31:37 +0200

    adding git lockfile checker

---
+++ src/Checks/GitLockCheck.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Concept7\Health\Checks;
+
+use Spatie\Health\Checks\Check;
+use Spatie\Health\Checks\Result;
+
+class GitLockCheck extends Check
+{
+    public function run(): Result
+    {
+        $result = Result::make();
+
+        $lockFile = base_path('.git/index.lock');
+
+        if (file_exists($lockFile)) {
+            $result->failed('Git Lock file detected. Fix this issue to enable sync.');
+        }
+
+        return $result->ok();
+    }
+}
